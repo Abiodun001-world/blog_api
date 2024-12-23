5d2ee75fa8ab8e9e53fe16099a911df7cab1d40c
Abiodun001-world <adekunleabiodun354@gmail.com>
2024-12-23 16:39:07 +0100

    fix: an entry route for a welcome message

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/blogs", blogRoutes);
 
+app.get("/", (req, res) => {
+    res.end(
+        "Hi, You are welcome to Abiodun Adejare Adekunle's AltSchool second semester exam (project based) Thanks for checking it out!")
+});
+
 module.exports = app;
