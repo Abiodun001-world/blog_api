4a523bda6027b3f54450d62fd6a36c2528e62462
Abiodun001-world <adekunleabiodun354@gmail.com>
2024-12-23 18:43:04 +0100

    fix: updated app.js file

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use("/blogs", blogRoutes);
 app.get("/", (req, res) => {
     res.end(
         "Hi, You are welcome to Abiodun Adejare Adekunle's AltSchool second semester exam (project based) Thanks for checking it out!" +
-        "\n\nCheck out the Postman documentation: <a href='https://www.postman.com/collections/your-postman-collection-link'>Postman Documentation: coming soon!</a>" +
-        "\n\nCheck out the GitHub repository: <a href='https://github.com/Abiodun001-world/blog_api'>GitHub Repository</a>"
+        "\n\nCheck out the Postman documentation: Postman Documentation: coming soon!" +
+        "\n\nCheck out the GitHub repository of this project:https://github.com/Abiodun001-world/blog_api"
     );
 });
 
